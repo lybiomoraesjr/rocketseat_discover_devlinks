d259ef591c4ba11d4924ca26594d1cae6c15be2e
Lybio Moraes Junior <lybiomoraesjr@gmail.com>
2024-01-12 21:10:08 -0300

    fix image paths

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "/assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute("alt", "light")
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "/assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute("alt", "dark")
   }
 }
